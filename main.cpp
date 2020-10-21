6d796abb1c49c634975d702d6ce5a72197e418b9
estebanrg21 <estebanrg9@gmail.com>
2020-10-21 12:45:17 -0600

    Ejemplos de la clase clientes
    En el main se encuentran los usos de la clase clientes usando solamente los metodos de la clase Menus

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
 #include "Menus.cpp"
+#include <conio.h>
+
 using namespace std;
-Cliente* crearCliente();
+
 int main (int argc, char *argv[]) {
-	guardarCliente();
+	
+	
+	//guardar cliente
+	//cout<<guardarCliente();
+	//cout<<"\nPress any key\n";getch();system("cls");
+	
+	//actualizar cliente
+	string b="";
+	int resp=stoi(validarInput(b,"[^0-9]","Inserte cedula a actualizar"));b="";
+	string campo=validarInput(b,"[^a-zA-Z ]","Campo a editar");b="";
+	string valor=validarInput(b,"[*?]","Valor a guardar");
+	cout<<actualizarCliente(resp,campo,valor);
+	cout<<"\nPress any key\n";getch();system("cls");
+	
+	//listar todos los clientes
+	cout<<"\n----Listar todos los clientes----\n";
+	cout<<listarClientes();
+	cout<<"\nPress any key\n";getch();system("cls");
+	
+	//listar todos los clientes activos
+	cout<<"\n----Listar clientes activos----\n";
+	cout<<listarClientesActivos();
+	cout<<"\nPress any key\n";getch();system("cls");
+	
+	//listar todos los clientes suspendidos
+	cout<<"\n----Listar clientes suspendidos----\n";
+	cout<<listarClientesSusp();
+	
+	
 	return 0;
 }
 
