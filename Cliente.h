eaed85ba8117bef8f121975027010938f98bb6d1
estebanrg21 <estebanrg9@gmail.com>
2020-10-21 12:31:58 -0600

    Agregado overload de << y constructor con json
    el overload del << es basicamente para cuando se quiera imprimir la clase, que directamente imprima el toString y con respecto al constructor con json object es basicamente para cuando se quiera listar los objetos dentro del archivo y crear objetos con los cuales se pueda interactuar despues

--- Cliente.h
+++ Cliente.h
@@ -1,7 +1,10 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 #include<iostream>
+#include "json.hpp"
 using namespace std;
+using json = nlohmann::json;
+
 class Cliente{
 private:
 	int cedula;
@@ -13,11 +16,13 @@ private:
 	string estado;
 	
 public:
+	Cliente(nlohmann::basic_json<> jdata,string key);
+	friend ostream& operator<<(ostream& os, const Cliente& dt);
 	Cliente();
 	~Cliente();
 	Cliente(int c,string n, string dir, int nt, int tel, string em, string es);
 	int** getPedidos();
-	string toString();
+	string toString() const;
 	int getCedula();
 	void setCedula(int cedula);
 	string getNombre();
