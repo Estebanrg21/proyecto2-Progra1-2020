2eb1045edfed034d7fb8900b3e08a9bc475a6183
estebanrg21 <estebanrg9@gmail.com>
2020-10-21 12:18:25 -0600

    Editado el return de los metodos para listar.\nAgregados los atributos instancias y mynum para controlar las instancias creadas.\nEditados los parametros del metodo actualizarCliente.

--- Clientes.h
+++ Clientes.h
@@ -2,17 +2,29 @@
 #define CLIENTES_H
 #include<iostream>
 #include "Cliente.h"
+#include "ListaEnlazada.hpp"
+#include "json.hpp"
+#include <fstream>
+#include <iomanip>
+#include <typeinfo> 
 using namespace std;
+using json = nlohmann::json;
+
 class Clientes{
 public:
 	Clientes();
 	~Clientes();
-	
-	Cliente* actualizarCliente();
+	static int instancias;
+	int mynum;
 	bool eliminarCliente(int ced);
-	Cliente** listarClientes();
-	Cliente** listarClientesActivos();
-	Cliente** listarClientesSusp();
-	string ingresarCliente(Cliente cl);
+	Lista<Cliente>listarClientes();
+	Lista<Cliente> listarClientesActivos();
+	Lista<Cliente> listarClientesSusp();
+	bool ingresarCliente(Cliente& cl);
+	string actualizarCliente(int ced,string field, string val);
+	
 };
 #endif
+
+
+
